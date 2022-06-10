696f7ef807469798dbb39967ee1f529b79bb78f2
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-11 00:16:14 +0530

    Refactored the code to edit the details of the address book

--- src/com/bridgelabz/address/AddressBook.java
+++ src/com/bridgelabz/address/AddressBook.java
@@ -62,5 +62,67 @@ public class AddressBook {
                 "\n Phone number : " + phoneNumber;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNo() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNumber = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/com/bridgelabz/address/AddressBookMain.java
+++ src/com/bridgelabz/address/AddressBookMain.java
@@ -3,9 +3,10 @@ package com.bridgelabz.address;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        AddressBook addressBook = new AddressBook();
+        EditAddressBook addressBook = new EditAddressBook();
         addressBook.readData();
         System.out.println(addressBook.addressBookDetail);
-
+        addressBook.editData();
+        System.out.println(addressBook.addressBookDetail);
     }
 }
